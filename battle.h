ea6cee796b1c0f7ceaea2af3996742f404e4e127
Piotr Kuczko <pk394397@students.mimuw.edu.pl>
2018-12-19 22:21:30 +0100

    Init

---
+++ battle.h
@@ -0,0 +1,8 @@
+//
+// Created by Piotrek on 19.12.2018.
+//
+
+#ifndef STARWARS2_BATTLE_H
+#define STARWARS2_BATTLE_H
+
+#endif //STARWARS2_BATTLE_H
---
+++ rebelfleet.h
@@ -0,0 +1,8 @@
+//
+// Created by Piotrek on 19.12.2018.
+//
+
+#ifndef STARWARS2_REBELFLEET_H
+#define STARWARS2_REBELFLEET_H
+
+#endif //STARWARS2_REBELFLEET_H
