41369ededadb694c65794fafa713b0552951b5f7
Piotr Kuczko <pk394397@students.mimuw.edu.pl>
2019-01-09 12:38:14 +0100

    gotowe

--- imperialfleet.h
+++ imperialfleet.h
@@ -5,55 +5,53 @@
 #include <memory>
 #include "rebelfleet.h"
 
-// https://stackoverflow.com/questions/274626/what-is-object-slicing
-
-/*
- * TODO: brak funkcji fabrykujących
- */
-
 class ImperialStarship {
 protected:
     size_t count = 1;
     ShieldPoints shield = -1;
     AttackPower power = -1;
-    ImperialStarship (ShieldPoints shield, AttackPower power) : shield(shield), power(power) {};
+
+    ImperialStarship(ShieldPoints shield, AttackPower power) : shield(shield), power(power) {};
+
     ImperialStarship() {};
 
 public:
-    size_t getCount () { return count; }
-    virtual ShieldPoints getShield() = 0;
+    size_t getCount() { return count; }
+
+    virtual ShieldPoints getShield() { return shield; };
+
     virtual AttackPower getAttackPower() { return power; }
-    virtual void takeDamage(AttackPower damage) { shield = (damage>=shield)? 0:shield-damage;
+
+    virtual void takeDamage(AttackPower damage) {
+        shield = (damage >= shield) ? 0 : shield - damage;
         if (shield == 0) count = 0;
-        std::cout << "klasyczna\n"; }
-    void show () {
-        std::cout << getShield() << " " << getAttackPower() << "\n";
     }
 };
 
 class DeathStar : public ImperialStarship {
 public:
-    ShieldPoints getShield () override { return shield; }
-    DeathStar (ShieldPoints shield, AttackPower power) : ImperialStarship(shield, power) {};
+    DeathStar(ShieldPoints shield, AttackPower power) : ImperialStarship(shield, power) {};
 };
-std::shared_ptr<DeathStar> createDeathStar (ShieldPoints shield, AttackPower power) {
+
+std::shared_ptr<DeathStar> createDeathStar(ShieldPoints shield, AttackPower power) {
     return std::make_shared<DeathStar>(shield, power);
 }
 
 class ImperialDestroyer : public ImperialStarship {
 public:
-    ShieldPoints getShield () override { return shield; }
-    ImperialDestroyer (ShieldPoints shield, AttackPower power) : ImperialStarship(shield, power) {};
+    ImperialDestroyer(ShieldPoints shield, AttackPower power) : ImperialStarship(shield, power) {};
 };
-std::shared_ptr<ImperialDestroyer> createImperialDestroyer (ShieldPoints shield, AttackPower power) {
+
+std::shared_ptr<ImperialDestroyer> createImperialDestroyer(ShieldPoints shield, AttackPower power) {
     return std::make_shared<ImperialDestroyer>(shield, power);
 }
+
 class TIEFighter : public ImperialStarship {
 public:
-    ShieldPoints getShield () override { return shield; }
-    TIEFighter (ShieldPoints shield, AttackPower power) : ImperialStarship(shield, power) {};
+    TIEFighter(ShieldPoints shield, AttackPower power) : ImperialStarship(shield, power) {};
 };
-std::shared_ptr<TIEFighter> createTIEFighter (ShieldPoints shield, AttackPower power) {
+
+std::shared_ptr<TIEFighter> createTIEFighter(ShieldPoints shield, AttackPower power) {
     return std::make_shared<TIEFighter>(shield, power);
 }
 
@@ -61,8 +59,10 @@ class Squadron : public ImperialStarship {
 private:
     std::vector<std::shared_ptr<ImperialStarship>> v;
 public:
-    Squadron (std::vector<std::shared_ptr<ImperialStarship>> v) : v(v) {getShield();};
-    Squadron (std::initializer_list<std::shared_ptr<ImperialStarship>> l) : v(l) {getShield();};
+    Squadron(std::vector<std::shared_ptr<ImperialStarship>> v) : v(v) { getShield(); };
+
+    Squadron(std::initializer_list<std::shared_ptr<ImperialStarship>> l) : v(l) { getShield(); };
+
     ShieldPoints getShield() override {
         if (shield == -1) {
             shield = 0;
@@ -74,6 +74,7 @@ public:
         }
         return shield;
     }
+
     AttackPower getAttackPower() override {
         if (power == -1) {
             power = 0;
@@ -83,22 +84,23 @@ public:
         }
         return power;
     }
+
     void takeDamage(AttackPower damage) override {
         shield = 0;
         count = 0;
         for (auto it : v) {
             it->takeDamage(damage);
             shield += it->getShield();
-            std::cout << "mam: " << it->getCount() << std::endl;
             count += it->getCount();
         }
-        std::cout << "zyjace stateczki: " << count << std::endl;
     }
 };
-std::shared_ptr<Squadron> createSquadron (std::vector <std::shared_ptr<ImperialStarship>> &v) {
+
+std::shared_ptr<Squadron> createSquadron(std::vector<std::shared_ptr<ImperialStarship>> &v) {
     return std::make_shared<Squadron>(v);
 }
-std::shared_ptr<Squadron> createSquadron (std::initializer_list <std::shared_ptr<ImperialStarship>> v) {
+
+std::shared_ptr<Squadron> createSquadron(std::initializer_list<std::shared_ptr<ImperialStarship>> v) {
     return std::make_shared<Squadron>(v);
 }
 
--- rebelfleet.h
+++ rebelfleet.h
@@ -4,13 +4,6 @@
 #include <iostream>
 #include <cassert>
 
-/*
- * TODO: brak funkcji fabrykujących
- * TODO: typy pomocnicze SHieldPoints, Speed, AttackPower
- */
-
-
-// do poprawy
 using ShieldPoints = int;
 using Speed = int;
 using AttackPower = int;
@@ -19,25 +12,32 @@ using AttackPower = int;
 class RebelStarship {
 protected:
     size_t count = 1;
-    virtual void checkSpeed(){};
+
+    virtual void checkSpeed() {};
     ShieldPoints shield = -1;
     Speed speed = -1;
     AttackPower power = -1;
-    RebelStarship (ShieldPoints shield, Speed speed, AttackPower power)
-            : shield(shield), speed(speed), power(power) {
-        std::cout << "RebelStarship: " << shield << " " << speed << " " << power << std::endl; checkSpeed();}
+
+    RebelStarship(ShieldPoints shield, Speed speed, AttackPower power)
+            : shield(shield), speed(speed), power(power) { checkSpeed(); }
+
     RebelStarship() {};
 public:
-    size_t getCount () { return count; }
-    ShieldPoints getShield() { return shield; } //zwraca wytrzymałość tarczy,
-    Speed getSpeed() { return speed; } //zwraca prędkość statku,
-    virtual void takeDamage(AttackPower damage) { shield = (damage>=shield)? 0:shield-damage;
+    size_t getCount() { return count; }
+
+    ShieldPoints getShield() { return shield; }
+
+    Speed getSpeed() { return speed; }
+
+    void takeDamage(AttackPower damage) {
+        shield = (damage >= shield) ? 0 : shield - damage;
         if (shield == 0) count = 0;
     }
+
     AttackPower getAttackPower() { return power; }
-    void show () {
-        std::cout << shield << " " << speed << " " << power << "\n";
-    }
+
+    virtual bool hasAttackPower() { return true; };
+    //void show () { std::cout << shield << " " << speed << " " << power << "\n"; }
 
 };
 
@@ -45,38 +45,47 @@ public:
 class Explorer : public RebelStarship {
 private:
 protected:
-    void checkSpeed() override { assert(299796<=speed && speed<=2997960); }
+    void checkSpeed() override { assert(299796 <= speed && speed <= 2997960); }
+
 public:
-    Explorer (ShieldPoints shield, Speed speed) {
-        RebelStarship::shield=shield; RebelStarship::speed=speed; checkSpeed(); };
+    Explorer(ShieldPoints shield, Speed speed) {
+        RebelStarship::shield = shield;
+        RebelStarship::speed = speed;
+        checkSpeed();
+    };
+
     AttackPower getAttackPower() = delete;
+
+    virtual bool hasAttackPower() override { return false; };
 };
 
-Explorer createExplorer (ShieldPoints shield, Speed speed) {
+Explorer createExplorer(ShieldPoints shield, Speed speed) {
     return Explorer(shield, speed);
 }
 
 class StarCruiser : public RebelStarship {
 private:
 protected:
-    void checkSpeed() override { assert(99999<=speed && speed<=299795); }
+    void checkSpeed() override { assert(99999 <= speed && speed <= 299795); }
+
 public:
-    StarCruiser (ShieldPoints shield, Speed speed, AttackPower power) : RebelStarship(shield, speed, power) {};
+    StarCruiser(ShieldPoints shield, Speed speed, AttackPower power) : RebelStarship(shield, speed, power) {};
 };
 
-StarCruiser createStarCruiser (ShieldPoints shield, Speed speed, AttackPower power) {
+StarCruiser createStarCruiser(ShieldPoints shield, Speed speed, AttackPower power) {
     return StarCruiser(shield, speed, power);
 }
 
 class XWing : public RebelStarship {
 private:
 protected:
-    void checkSpeed() override { assert(299796<=speed && speed<=2997960); }
+    void checkSpeed() override { assert(299796 <= speed && speed <= 2997960); }
+
 public:
-    XWing (ShieldPoints shield, Speed speed, AttackPower power) : RebelStarship(shield, speed, power) {};
+    XWing(ShieldPoints shield, Speed speed, AttackPower power) : RebelStarship(shield, speed, power) {};
 };
 
-XWing createXWing (ShieldPoints shield, Speed speed, AttackPower power) {
+XWing createXWing(ShieldPoints shield, Speed speed, AttackPower power) {
     return XWing(shield, speed, power);
 }
 
